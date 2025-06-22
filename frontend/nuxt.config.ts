81899338291515d6bfa7dcb6f8c410fe35f7be45
Ruthvik101 <ruthvikmt001@gmail.com>
2025-06-22 13:23:43 +0530

    hi.json/hindi-translation

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -10,25 +10,33 @@ export default defineNuxtConfig({
   app: {
     head,
   },
+
   modules: modules,
   ssr: false,
+
   typescript: {
     // strict: true,
     // typeCheck: true,
   },
+
   devtools: {
     enabled: true,
   },
+
   alias: {
     "@": resolve(__dirname, "./"),
   },
+
   plugins: ["~/plugins/i18n-head.ts"],
+
   content: {
     watch: { enabled: false },
   },
+
   imports: {
     dirs: ["./stores"],
   },
+
   vite: {
     server: {
       watch: {
@@ -43,20 +51,25 @@ export default defineNuxtConfig({
       },
     },
   },
+
   colorMode: {
     classSuffix: "",
   },
+
   css: ["reduced-motion/css"],
+
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
     configPath: "tailwind.config.ts",
   },
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   i18n: {
     lazy: true,
     strategy: "prefix_and_default",
@@ -72,24 +85,30 @@ export default defineNuxtConfig({
       redirectOn: "root",
     },
   },
+
   components: [
     {
       path: "~/components",
       global: true,
     },
   ],
+
   vue: {
     compilerOptions: {
       isCustomElement: (tag) =>
         ["swiper-slide", "swiper-container"].includes(tag),
     },
   },
+
   hooks: {
     "app:resolve": (app) => {
       console.log("App instance resolved:", app);
     },
   },
+
   nitro: {
     preset: "netlify-static",
   },
+
+  compatibilityDate: "2025-06-22",
 });
