21ed5a1170ce0e08895e3505b8429104c9fb6a6e
akshayw1 <ie2022117@iiita.ac.in>
2025-06-29 22:27:24 +0530

    feat: added localizations for malayalam,nepali,telgu,tamil

--- frontend/locales.ts
+++ frontend/locales.ts
@@ -2,11 +2,19 @@
 export enum LOCALE_NAME {
   ENGLISH = "English",
   HINDI = "हिन्दी",
+  MALAYALAM = "മലയാളം",
+  NEPALI = "नेपाली",
+  TAMIL = "தமிழ்",
+  TELUGU = "తెలుగు",
 }
 
 export enum LOCALE_CODE {
   ENGLISH = "en",
   HINDI = "hi",
+  MALAYALAM = "ml",
+  NEPALI = "ne",
+  TAMIL = "ta",
+  TELUGU = "te",
 }
 
 export interface Locale {
@@ -30,6 +38,34 @@ export const locales: Locale[] = [
     language: "hi",
     name: LOCALE_NAME.HINDI,
     file: "../hi.json",
+    isCatchallLocale: false,
+  },
+  {
+    code: LOCALE_CODE.MALAYALAM,
+    language: "ml",
+    name: LOCALE_NAME.MALAYALAM,
+    file: "../ml.json",
+    isCatchallLocale: false,
+  },
+  {
+    code: LOCALE_CODE.NEPALI,
+    language: "ne",
+    name: LOCALE_NAME.NEPALI,
+    file: "../ne.json",
+    isCatchallLocale: false,
+  },
+  {
+    code: LOCALE_CODE.TAMIL,
+    language: "ta",
+    name: LOCALE_NAME.TAMIL,
+    file: "../ta.json",
+    isCatchallLocale: false,
+  },
+  {
+    code: LOCALE_CODE.TELUGU,
+    language: "te",
+    name: LOCALE_NAME.TELUGU,
+    file: "../te.json",
     isCatchallLocale: false,
   },
 ];
