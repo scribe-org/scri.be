e1f09b166a05d5667620a187bf160c0e99031b68
Purnama S Rahayu <purnamasrahayu@gmail.com>
2025-08-18 19:39:01 +0700

    [feature] - create Android and iOS changelog separately #5

---
+++ frontend/pages/docs/conjugate/ios-changelog.vue
@@ -0,0 +1,12 @@
+<!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
+<template>
+  <PageDocs
+    imgURL="/icons/docs/bootstrap_file_code"
+    imgAltText="i18n.pages.docs._global.changelog.file_code_img_alt_text"
+  >
+    <PageBreadcrumbs />
+    <h1 class="pb-2 font-bold">
+      {{ $t("i18n._global.ios_changelog") }}
+    </h1>
+  </PageDocs>
+</template>
