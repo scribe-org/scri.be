364bdcb16588c50eb24e2a51df49bef84504a28a
henrikth93 <henrikthomasson93@gmail.com>
2025-10-19 21:50:22 +0200

    Comply with linting
    This commit will fix linting issues.

--- frontend/app/pages/docs/server/available-data.vue
+++ frontend/app/pages/docs/server/available-data.vue
@@ -11,50 +11,68 @@
     </h1>
 
     <p>{{ $t("i18n.pages.docs.server.available_data.under_development") }}</p>
-    <div class="flex justify-center my-6">
+    <div class="my-6 flex justify-center">
       <div class="w-full max-w-4xl overflow-x-auto">
         <table class="min-w-full divide-y divide-gray-200">
           <thead class="bg-gray-50">
             <tr>
-              <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Languages</th>
-              <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nouns</th>
-              <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Verbs</th>
+              <th
+                class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+              >
+                Languages
+              </th>
+              <th
+                class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+              >
+                Nouns
+              </th>
+              <th
+                class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+              >
+                Verbs
+              </th>
             </tr>
           </thead>
-          <tbody class="bg-white divide-y divide-gray-200">
+          <tbody class="divide-y divide-gray-200 bg-white">
             <tr v-for="(item, index) in languageData" :key="index">
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.language_name }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.nouns }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.verbs }}</td>
+              <td class="whitespace-nowrap px-6 py-4">
+                {{ item.language_name }}
+              </td>
+              <td class="whitespace-nowrap px-6 py-4">
+                {{ item.nouns }}
+              </td>
+              <td class="whitespace-nowrap px-6 py-4">
+                {{ item.verbs }}
+              </td>
             </tr>
           </tbody>
         </table>
       </div>
     </div>
   </PageDocs>
-
 </template>
 
 <script>
 export default {
-data() {
-  return {
-    languageData: [],
-    error: null
-  }
-},
-async mounted() {
-  try {
-  const response = await fetch('https://scribe-server.toolforge.org/api/v1/language-stats');
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json();
-    this.languageData = data || [];
+  data() {
+    return {
+      languageData: [],
+      error: null,
+    };
+  },
+  async mounted() {
+    try {
+      const response = await fetch(
+        "https://scribe-server.toolforge.org/api/v1/language-stats"
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      this.languageData = data || [];
     } catch (error) {
       this.error = error.message;
-      console.error('Error fetching data:', error);
     }
-  }
-}
+  },
+};
 </script>
